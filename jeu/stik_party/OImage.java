72ffbcabb244885b6456ac32ca0221acdc1641e5
karchnu <kane.root@gmail.com>
2012-03-10 12:34:45 +0100

    Première entre-vue entre collabos

---
+++ jeu/stik_party/OImage.java
@@ -0,0 +1,3 @@
+public class OImage extends BufferedImage
+{
+}
---
+++ jeu/stik_party/Objets.java
@@ -0,0 +1,8 @@
+public abstract class Objet
+{
+	public Point p;
+	public int hauteur, largeur;
+
+	public boolean estEnCollision(Objet);
+	public OImage affiche();
+}
