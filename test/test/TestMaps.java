921637741de9819cefd31a6232da154aa636ee30
karchnu <kane.root@gmail.com>
2012-04-04 17:42:03 +0200

    Listage de maps

--- test/test/TestMaps.java
+++ test/test/TestMaps.java
@@ -1,14 +1,20 @@
 package test;
 
 import java.io.*;
+import java.util.ArrayList;
+
+import modele.Map;
 import util.ChargementMap;
+import util.ListeMaps;
 
 public class TestMaps
 {
 	public static void main(String[] args)
 	{
 		//ChargementMap c = new ChargementMap(args[0]);
-		File f = new File("./maps");
-		TestMaps.printContent(f, 0);
+		ListeMaps lesmaps = new ListeMaps("./maps");
+		ArrayList<Map> ml = lesmaps.getListe();
+		for(Map m : ml)
+			System.out.println("Une map de plus");
 	}
 }
