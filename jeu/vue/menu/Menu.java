1a22f9246a0b6568e860f9ebe394f5ee62f505d3
karchnu <kane.root@gmail.com>
2012-04-05 19:17:32 +0200

    voilà, ajout du bouton le plus utile au monde

--- jeu/vue/menu/Menu.java
+++ jeu/vue/menu/Menu.java
@@ -10,17 +10,20 @@ public class Menu extends VueMenu
 {
 	public JButton jb_partie = new JButton("Nouvelle partie");
 	public JButton jb_recommencer = new JButton("Continuer");
+	public JButton jb_map = new JButton("Choix de la carte !");
 
 	public Menu(ControleurMenu c)
 	{
 		super(c);
 
-		this.jb_partie.addActionListener(new JBStartGameListener(this.getControleur(), this));
-		this.jb_recommencer.addActionListener(new JBResumeListener(this.getControleur(), this));
+		jb_partie.addActionListener(new JBStartGameListener(this.getControleur(), this));
+		jb_recommencer.addActionListener(new JBResumeListener(this.getControleur(), this));
 
-		JLabel jl_titre = new JLabel("Bienvenue sur le jeu ! Il est cool, vraiment.");
+		JLabel jl_titre = new JLabel("POUR RAPPEL CE JEU EST TRÈS BIEN.");
+		this.setLayout(new BorderLayout());
 		this.add(jl_titre,BorderLayout.NORTH);
-		this.add(jb_partie,BorderLayout.SOUTH);
+		this.add(jb_partie,BorderLayout.CENTER);
+		this.add(jb_map, BorderLayout.EAST);
 		this.add(jb_recommencer,BorderLayout.SOUTH);
 	}
 }
