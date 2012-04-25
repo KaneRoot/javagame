87c49e0b66ae35d41f561eff3550a3b8c64805b4
lubuntu <kilian@ubuntu.(none)>
2012-04-25 15:22:48 -0700

    Ajout classe E_lethal

---
+++ jeu/modele/elements/E_lethal.java
@@ -0,0 +1,26 @@
+package modele.element;
+
+
+
+public class E_lethal extends Element
+{
+	public E_lethal (int x, int y, int size)
+	{
+		super (x,y,size);
+		setBounding(new Bounding(new Ellipse(getPosition(),size+10,size+10)));
+		setEtat (PLEINEVIE);
+	}
+
+	public boolean entreEnCollision(Element el)
+	{
+		if  (bounding_.estEnCollision(el.getBounding()))
+		{
+			el.setEtat(MORT);
+			collision();
+			return true;
+		}
+		else
+			return false;
+	}
+
+}
