f4248d0fbf0239ce642a5461ef90b5ab9fc12488
karchnu <kane.root@gmail.com>
2012-04-27 12:37:03 +0200

    ajout d'un sacré bout de documentation

--- jeu/modele/Map.java
+++ jeu/modele/Map.java
@@ -8,6 +8,13 @@ import modele.listener.MouvementListener;
 import util.Point2d;
 import modele.elements.*;
 
+
+/**
+ * Carte du jeu.
+ *
+ * Contient les éléments affichés, ainsi que le personnage.
+ */
+
 public class Map
 {
 	private ArrayList<Point2d> sol_;
@@ -21,7 +28,7 @@ public class Map
 	private String nom = null;
 
 	/**
-	 * Ce constructeur va servir pour faire une copie de la Map courante.
+	 * Ce constructeur va servir pour faire une copie de la carte courante.
 	 *
 	 */
 	public Map(int w, int h, int masse, E_perso e, ArrayList<Point2d> sol, ArrayList<Element> elements, String nom, EventListenerList ecouteurs)
@@ -44,18 +51,38 @@ public class Map
 		ecouteurs_ = new EventListenerList();
 	}
 
+	/**
+	 * Constructeur par défaut.
+	 */
 	public Map()
 	{
 		this(0,0,null);
 	}
 	
+	/** Défini tous les points du sol en une fois. */
 	public void setSol(ArrayList<Point2d> sol) { this.sol_ = sol; }
+
+	/** Défini tous les écouteurs de la carte en une fois. */
 	public void setEcouteurs(EventListenerList ecouteurs) { this.ecouteurs_ = ecouteurs; }
+
+	/** Défini l'arrivée. */
 	public void setArrivee(int a) { this.arrivee = a; }
+
+	/** Défini le nom de la carte. */
 	public void setNom(String lenom) { this.nom = lenom; }
+
+	/** Renvoie le nom de la carte. */
 	public String getNom() { return this.nom; }
+
+	/** Renvoie la coordonnée X de l'arrivée en fin de partie. */
 	public int getArrivee() { return this.arrivee; }
 
+	/** 
+	 * Ajout d'un écouteur du mouvement. 
+	 * 
+	 * @param ml : va écouter le mouvement.
+	 *
+	 */
 	public void ajouterEcouteurMouvement (MouvementListener ml)
 	{
 		ecouteurs_.add(MouvementListener.class, ml);
@@ -66,18 +93,43 @@ public class Map
 		ecouteurs_.remove(MouvementListener.class, ml);
 	}
 
+	/** 
+	 * Ajout d'un élément. 
+	 * 
+	 * @param e : l'élément à ajouter. 
+	 * 
+	 */
 	public void addElem(Element e) { elem_.add(e); }
 
+	/** Renvoie le nombre d'éléments de la carte. */
 	public int nbElem()	{ return elem_.size(); }
 
+	/** Renvoie le énième élément de la carte. */
 	public Element getElem(int i) { return elem_.get(i); }
 
+	/** 
+	 * Ajoute un point du sol.
+	 * 
+	 * @param sol : les coordonnées d'un point à relier aux autres pour former le sol.
+	 *
+	 */
 	public void addPoint(Point2d sol) { sol_.add(sol); }
 	
+	/** Renvoie le nombre de points formant le sol. */
 	public int nbPointSol() { return sol_.size(); }
 	
+	/** 
+	 * Renvoie le énième point du sol. 
+	 * 
+	 * @param i : le énième point qui forme le sol.
+	 *
+	 * */
 	public Point2d getPointSolI(int i) { return sol_.get(i); }
 	
+	/** 
+	 * Renvoie la hauteur du sol à une coordonnée X passée en paramètre. 
+	 * @param x : la coordonnée des abscisses.
+	 * */
 	public int getYSol(int x) 
 	{
 		int i;
@@ -89,21 +141,34 @@ public class Map
 		return (int)(r*(x-sol_.get(i).getX()) + sol_.get(i).getY());
 	}
 
+	/** Défini la largeur de la carte. */
 	public void setW(int w) { this.w_ = w; }
 
+	/** Renvoie la largeur de la carte. */
 	public int getW() { return w_; }
 
+	/** Définie la hauteur de la carte. */
 	public void setH(int h) { this.h_ = h; }
 
+	/** Renvoie la hauteur de la carte. */
 	public int getH() { return h_; }
 
+	/** Définie le personnage de la carte. */
 	public void setPerso(E_perso stick) { this.perso_ = stick; }
 
+	/** Renvoie le personnage de la carte. */
 	public E_perso getPerso() { return perso_; }
 
+	/** Modifie la masse de la carte. */
 	public void setMasse(int masse) { this.masse = masse; }
 
+	/** Renvoie la masse de la carte. */
 	public int getMasse() { return masse; }
+
+	/**
+	 * Proposera à terme de renvoyer une copie de la carte.
+	 *
+	 */
 
 	public Map copie()
 	{
