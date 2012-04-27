ba2ed7ead8c8ae0883848237cabefdde4ef21f3f
karchnu <kane.root@gmail.com>
2012-04-27 16:33:16 +0200

    ajout de doc

--- jeu/modele/listener/CollisionListener.java
+++ jeu/modele/listener/CollisionListener.java
@@ -3,7 +3,9 @@ package modele.listener;
 import java.util.EventListener;
 import modele.event.CollisionEvent;
 
+/** interface qui défini les classes nécessaires pour être un écouteur de collision. */
 public interface CollisionListener extends EventListener
 {
+	/** Totally wtf ? */
 	public void enCollision(CollisionEvent event);
 }
